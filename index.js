9a8bb05b94c1578ae1169e5bd842d1c737907620
nagarjuna017 <nagarjuna.punna@zemosolabs.com>
2022-01-10 19:23:52 +0530

    Commited

---
+++ index.js
@@ -0,0 +1,36 @@
+let localP = document.getElementById("local");
+let localS = document.getElementById("session");
+
+
+function setInitialData() {
+    if (localStorage.getItem("localCounter")) {
+        localP.innerHTML = ": " + localStorage.getItem("localCounter");
+        localS.innerHTML = ": " + 0;    
+    }
+    else {
+        localP.innerHTML = ": " + 0;
+        localS.innerHTML = ": " + 0;
+    }
+}
+
+function lStorage() {
+    if (localStorage.getItem("localCounter")) {
+        localP.innerHTML = ": " + (parseInt(localStorage.getItem("localCounter")) + 1)
+        localStorage.setItem("localCounter", parseInt(localStorage.getItem("localCounter")) + 1);
+    }
+    else {
+        localP.innerHTML = ": " + 1;
+        localStorage.setItem("localCounter", 1);
+    }
+}
+
+function sStorage() {
+    if (sessionStorage.getItem("sessionCounter")) {
+        localS.innerHTML = ": " + (parseInt(sessionStorage.getItem("sessionCounter")) + 1)
+        sessionStorage.setItem("sessionCounter", parseInt(sessionStorage.getItem("sessionCounter")) + 1);
+    }
+    else {
+        localS.innerHTML = ": " + 1;
+        sessionStorage.setItem("sessionCounter", 1);
+    }
+}
